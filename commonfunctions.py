be15ce03695949ef34bc91025cbbc721838140ee
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2020-12-14 13:52:11 +0200

    Initial Code

---
+++ commonfunctions.py
@@ -0,0 +1,39 @@
+
+
+import skimage.io as io
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.exposure import histogram
+from matplotlib.pyplot import bar
+from skimage.color import rgb2gray
+
+# Show the figures / plots inside the notebook
+
+
+def show_images(images, titles=None):
+    # This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
+    # images[0] will be drawn with the title titles[0] if exists
+    # You aren't required to understand this function, use it as-is.
+    n_ims = len(images)
+    if titles is None:
+        titles = ['(%d)' % i for i in range(1, n_ims + 1)]
+    fig = plt.figure()
+    n = 1
+    for image, title in zip(images, titles):
+        a = fig.add_subplot(1, n_ims, n)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+        plt.axis('off')
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+    plt.show()
+
+
+def showHist(img):
+    # An "interface" to matplotlib.axes.Axes.hist() method
+    plt.figure()
+    imgHist = histogram(img, nbins=256)
+
+    bar(imgHist[1].astype(np.uint8), imgHist[0], width=0.8, align='center')
