d01928345fd7718f5767503c0fe1daa8bd35c0e3
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-01-08 23:18:04 +0200

    Manipulate the data using python script

---
+++ data.py
@@ -0,0 +1,25 @@
+from commonfunctions import *
+from glob import  glob
+import os
+from PIL import Image
+
+def inv(img_path, save_path):
+    img = io.imread(img_path)
+    img = rgb2gray(img)
+    bw = (255*(1 - get_thresholded(img, threshold_otsu(img)))).astype(np.uint8)
+    io.imsave(save_path, bw)
+
+dir_name = 'Eight'
+imgs_path = glob(f'train_data/data/natural/nat/*.bmp')
+imgs_path = sorted(imgs_path)
+res_path = f'train_data/data/#/sh'
+imgs_names = [img.split('/')[3].split('.')[0] for img in imgs_path]
+
+for i, img in enumerate(imgs_path):
+    im = Image.open(img)
+    im.save(f'train_data/data/cross/{imgs_names[i]}.png')
+    os.remove(img)
+
+os.mkdir(res_path)
+for i, img in enumerate(imgs_path):
+    inv(img, res_path+'/'+imgs_names[i])
