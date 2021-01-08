c413e3e66103c7982f4ca3e755b04c2d18711ce5
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-01-09 00:54:22 +0200

    Train SVM, KNN, NN classifiers and choose the best accuracy

--- data.py
+++ data.py
@@ -1,13 +1,15 @@
 from commonfunctions import *
-from glob import  glob
+from glob import glob
 import os
 from PIL import Image
+
 
 def inv(img_path, save_path):
     img = io.imread(img_path)
     img = rgb2gray(img)
     bw = (255*(1 - get_thresholded(img, threshold_otsu(img)))).astype(np.uint8)
     io.imsave(save_path, bw)
+
 
 dir_name = 'Eight'
 imgs_path = glob(f'train_data/data/natural/nat/*.bmp')
@@ -23,3 +25,12 @@ for i, img in enumerate(imgs_path):
 os.mkdir(res_path)
 for i, img in enumerate(imgs_path):
     inv(img, res_path+'/'+imgs_names[i])
+
+
+dirs = [path for path in glob(f'train_data/data/*')]
+dir_names = [path.split('/')[2] for path in dirs]
+
+for i, directory in enumerate(dirs):
+    imgs = glob(f'{directory}/*.png')
+    for j, img in enumerate(imgs):
+        os.rename(img, f'{directory}/{dir_names[i]}_{j}.png')
