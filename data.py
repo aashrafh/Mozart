d1d0e9eea118d24c9477f31b892465d31bf013b1
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-01-09 03:44:37 +0200

    Retrain SVM model after modifying the dataset. Accuracy 94.5%

--- data.py
+++ data.py
@@ -4,33 +4,31 @@ import os
 from PIL import Image
 
 
-def inv(img_path, save_path):
+def inv(img_path):
     img = io.imread(img_path)
     img = rgb2gray(img)
     bw = (255*(1 - get_thresholded(img, threshold_otsu(img)))).astype(np.uint8)
-    io.imsave(save_path, bw)
+    return bw
 
 
-dir_name = 'Eight'
-imgs_path = glob(f'train_data/data/natural/nat/*.bmp')
-imgs_path = sorted(imgs_path)
-res_path = f'train_data/data/#/sh'
-imgs_names = [img.split('/')[3].split('.')[0] for img in imgs_path]
+for dir_n in ['p']:
+    imgs = glob(f'train_data/Dataset/{dir_n}/*.png')
+    for img in imgs:
+        bw = inv(img)
+        os.remove(img)
+        io.imsave(img, bw)
 
-for i, img in enumerate(imgs_path):
-    im = Image.open(img)
-    im.save(f'train_data/data/cross/{imgs_names[i]}.png')
-    os.remove(img)
+# for dir_n in glob('train_data/Dataset/*'):
+#     for img in glob(f'{dir_n}/*'):
+#         name = img.split('.')[0]
+#         extension = img.split('.')[1]
+#         if extension != 'png' and extension != 'db':
+#             im = Image.open(img)
+#             im.save(f'{name}.png')
 
-os.mkdir(res_path)
-for i, img in enumerate(imgs_path):
-    inv(img, res_path+'/'+imgs_names[i])
-
-
-dirs = [path for path in glob(f'train_data/data/*')]
-dir_names = [path.split('/')[2] for path in dirs]
-
-for i, directory in enumerate(dirs):
-    imgs = glob(f'{directory}/*.png')
-    for j, img in enumerate(imgs):
-        os.rename(img, f'{directory}/{dir_names[i]}_{j}.png')
+# for dir_n in glob('train_data/Dataset/*'):
+#     for img in glob(f'{dir_n}/*'):
+#         name = img.split('.')[0]
+#         extension = img.split('.')[1]
+#         if extension != 'png':
+#             os.remove(img)
